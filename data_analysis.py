2f4c242b6b99d7a57926689ac86e12acab3fa36e
Carlos Catarino <carlos_cata27@hotmail.com>
2021-07-29 16:09:31 -0500

    Update

---
+++ data_analysis.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+archive = pd.read_excel('RED-NEGATIVA-COVID-02_VIGILANCIA-HOSPITALARIA 11.01.21.xlsx')
+head =archive.head() 
+print(head)
